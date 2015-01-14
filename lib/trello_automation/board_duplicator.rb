14f28b8fbf03c831b301a827722427a4ba79e4c3
mateusz_kmiecinski <m.kmiecinski@pilot.co>
2015-01-14 20:30:21 +0100

    add basic duplication

--- lib/trello_automation/board_duplicator.rb
+++ lib/trello_automation/board_duplicator.rb
@@ -5,7 +5,25 @@ class BoardDuplicator
   def call
     Authorization.authorize
 
-    board = Trello::Board.find('w9r5icGE')
+    board = Trello::Board.find('Fqvqdqeo')
+
+    duplicatedBoard = Trello::Board.create({name: "#{board.name}-mkm",
+                                            organization_id: board.organization_id})
+
+    duplicatedBoard.lists.each { |list| list.close! }
+
+    board.lists.reverse.each do |list|
+      duplicatedList = Trello::List.create({name: list.name,
+                                            board_id: duplicatedBoard.id})
+
+      list.cards.each do |card|
+        Trello::Card.create({name: card.name,
+                             list_id: duplicatedList.id,
+                             desc: card.desc})
+      end
+
+    end
+
   end
 
 end
