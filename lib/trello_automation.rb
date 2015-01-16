040fc3161efc6b487fa9135b19820178a72d87be
mateusz_kmiecinski <m.kmiecinski@pilot.co>
2015-01-16 11:45:52 +0100

    pass arguments to duplicator; duplicate boards for many members

--- lib/trello_automation.rb
+++ lib/trello_automation.rb
@@ -3,10 +3,29 @@ require 'trello_automation/board_duplicator'
 
 class TrelloAutomation
 
-  def start
+  def start(arg)
     bd = BoardDuplicator.new
-    bd.call('https://trello.com/b/Fqvqdqeo/workshop-4-ruby',
-            member_name: 'psulkowski',
-            full_member_name: 'Paweł Sułkowski')
+    board_url = arg[0]
+    members_list_path = arg[1]
+    if members_list_path
+      names(members_list_path).each do |full_member_name, member_name|
+        bd.call(board_url,
+                member_name: member_name,
+                full_member_name: full_member_name)
+      end
+    else
+      bd.call(board_url)
+    end
+  end
+
+  private
+
+  def names(members_list_path)
+    names = []
+    File.open(members_list_path, encoding: 'utf-8').each do |line|
+      /^(?<full_member_name>.*)<(?<member_name>.*)>$/ =~ line
+      names << [full_member_name, member_name]
+    end
+    names
   end
 end
