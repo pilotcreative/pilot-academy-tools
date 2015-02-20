163d682fcebdf66b7d86cbeb9e938545223c0818
mdkalish <mdkalish4git@gmail.com>
2015-02-20 20:09:06 +0100

    Refactor: WIP

--- lib/trello_automation/board_duplicator.rb
+++ lib/trello_automation/board_duplicator.rb
@@ -7,7 +7,7 @@ class BoardDuplicator
     board, board_users_copy = get_boards_array(board_url, options)
     board_users_copy.lists.each { |list| list.close! }
     board.lists.reverse.each do |list|
-      @done_list_id = list.id if list.name =~ /done/i
+      @done_list = list if list.name =~ /done/i
       list_copy = Trello::List.create(name: list.name, board_id: board_users_copy.id)
       list.cards.reverse.each do |card|
         Trello::Card.create(name: card.name,
@@ -18,7 +18,7 @@ class BoardDuplicator
     # p board_users_copy.lists.map(&:name).any? { |e| e =~ /done/i }
     # p board_users_copy.lists.find()
     # p board_users_copy.lists.pluck(:name).include?(/done/i)
-    subscribe_me_to_users_done_list(@done_list_id)
+    subscribe_me_to_users_done_list(@done_list, board_users_copy.id, options[:full_member_name])
     add_member(board_users_copy.id, options[:member_nickname]) if options[:member_nickname]
   end
 
@@ -34,15 +34,26 @@ class BoardDuplicator
 
   def board_token(board_url)
     %r{.*trello.com/b/(?<token>.*)/.*} =~ board_url
+    # %r{.*trello.com/b/(?<token>.*)/.*} =~ 'https://trello.com/b/khsx7Ez1/tat'
     token
   end
 
-  def subscribe_me_to_users_done_list(done_list_id)
-    # p board_users_copy.lists.where(:name, 'done')
-    if done_list_id
-      subscribe_member(done_list_id)
+  def close_boards(client, array_with_tokens)
+    array_with_tokens.each do |token|
+      client.put("/boards/#{token}", {closed: true})
+      puts "Closed board #{token}."
     end
-      puts done_list_copy.name
+  end
+
+  def subscribe_me_to_users_done_list(done_list, board_id = '', member = '')
+    if done_list
+      subscribe_member(done_list.id)
+    else
+      done_list = Trello::List.create(name: 'Done', board_id: board_id)
+      subscribe_member(done_list.id)
+    end
+    puts "You have been subscribed to the list '#{done_list.name}' of user #{member.strip}."
+    p done_list.board_id
   end
 
   def add_member(board_id, member_nickname)
@@ -52,10 +63,23 @@ class BoardDuplicator
     client.put(path, type: 'normal')
   end
 
-  def subscribe_member(done_list_copy_id)
+  def subscribe_member(done_list_id)
     client = Trello.client
-    path = "/lists/#{done_list_copy_id}/subscribed"
+    path = "/lists/#{done_list_id}/subscribed"
     client.put(path, value: true)
+    # p client
+    # inspect
+    # client.put("/boards/xdJ6Icuq", {closed: true})
+    # %r{.*trello.com/b/(?<token>.*)/.*} =~ client.get("/members/me/boards").to_hash
+    my_board_tokens = client.get("/members/me/boards").scan(/(?<="shortLink":")(.{8})/).flatten
+    2.times { my_board_tokens.delete_at(0) }
+    close_boards(client, my_board_tokens)
+
+    # boards = Trello::Board.find("DSLZcAdN").attributes[:closed]
+    # p boards
     # p client.put(path, value: true) #> "{\"id\":\"54e73d680cab5d847df6ad59\",\"name\":\"done\",\"closed\":false,\"idBoard\":\"54e73d659d32fdbcee6b73ab\",\"pos\":8192}"
   end
 end
+
+
+# close a board: client.put("/boards/xdJ6Icuq", {closed: true})
