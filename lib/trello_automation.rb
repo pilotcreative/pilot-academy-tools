0ef5454bea9f8c42cd096c7016ae9e7ea96b4904
mdkalish <mdkalish4git@gmail.com>
2015-02-24 17:07:39 +0100

    Improve subscribing: create list if needed. (WIP)

--- lib/trello_automation.rb
+++ lib/trello_automation.rb
@@ -8,10 +8,13 @@ class TrelloAutomation
       board_duplicator.close_boards(arg[1] ||= 'starred')
     else
       board_url, members_list_path = arg
+      i = 1
       if members_names = names(members_list_path)
-        members_names.each do |full_member_name, member_nickname|
+        members_names.each do |full_member_name, trello_nickname|
+          puts "\nCreating board copy #{i}..."
+          i += 1
           board_duplicator.call(board_url,
-                                member_nickname: member_nickname,
+                                trello_nickname: trello_nickname,
                                 full_member_name: full_member_name)
         end
       else
@@ -26,8 +29,8 @@ class TrelloAutomation
     return false if members_list_path.nil?
     names = []
     File.open(members_list_path, encoding: 'utf-8').each do |line|
-      /^(?<full_member_name>.*)<(?<member_nickname>.*)>$/ =~ line
-      names << [full_member_name, member_nickname]
+      /^(?<full_member_name>.*)<(?<trello_nickname>.*)>$/ =~ line
+      names << [full_member_name, trello_nickname]
     end
     # p names #> [["John Doe ", "johnny"], ["Lorem Ipsum ", "cicero"]]
     names
--- lib/trello_automation/board_duplicator.rb
+++ lib/trello_automation/board_duplicator.rb
@@ -4,26 +4,12 @@ LISTS_TO_SUBSCRIBE_TO = ['Done'] # case insensitive
 
 class BoardDuplicator
   def call(board_url, options = {})
-    # p options #>  {:member_nickname=>"johnny", :full_member_name=>"John Doe "}
+    # p options #>  {:trello_nickname=>"johnny", :full_member_name=>"John Doe "}
     Authorization.authorize
     original_board = find_board(board_url)
-    cloned_board = clone_board(original_board, options[:full_member_name])
-    # cloned_board = find_board(JSON.parse(cloned_board)['url'])
-    # close_default_lists_in(cloned_board)
-    # clone_lists_from_to(original_board, cloned_board.id)
-    # clone_cards_from_to(original_board, cloned_board)
-
-    # board.lists.reverse.each do |list|
-    #   @done_list = list if LISTS_TO_SUBSCRIBE_TO.each{ |e| e.downcase! }.include?(list.name.downcase!)
-    #   list_copy = Trello::List.create(name: list.name, board_id: board_users_copy.id)
-    #   list.cards.reverse.each do |card|
-    #     Trello::Card.create(name: card.name,
-    #                         list_id: list_copy.id,
-    #                         desc: card.desc)
-    #   end
-    # end
-    # subscribe_me_to_users_done_list(@done_list, board_users_copy.id, options[:full_member_name])
-    # add_member(board_users_copy.id, options[:member_nickname]) if options[:member_nickname]
+    cloned_board_id = clone_board(original_board, options[:full_member_name])
+    subscribe_me_to_chosen_lists_in(cloned_board_id)
+    add_member(cloned_board_id, options[:trello_nickname]) if options[:trello_nickname]
   end
 
   def close_boards(filter = '')
@@ -31,8 +17,10 @@ class BoardDuplicator
     tokens_of_boards_to_close = []
     leave_out(filter).each { |e| tokens_of_boards_to_close << e['shortLink'] }
     tokens_of_boards_to_close.each do |token|
-      name = JSON.parse(client.put("/boards/#{token}", {closed: true}))['name']
-      puts "Closed board #{name.strip} (shortLink: #{token})."
+      closed_board = JSON.parse(client.put("/boards/#{token}", closed: true))
+      name = closed_board['name']
+      url = closed_board['url']
+      puts "Closed board #{name}, URL: #{url}."
     end
   end
 
@@ -45,11 +33,9 @@ class BoardDuplicator
   def clone_board(original_board, clone_name)
     clone_name ||= 'copy'
     name = "#{original_board.name} - #{clone_name}".strip
-    # cloned_board = client.post('/boards', name: name,
-    #                organization_id: original_board.organization_id)
-    # puts "Created a copy of #{original_board.name} with the name #{name}."
-    # cloned_board
-    client.post('/boards', name: name, idBoardSource: original_board.id)
+    cloned_board = client.post('/boards', name: name, idBoardSource: original_board.id)
+    puts "Created a copy of #{original_board.name} with the name #{name}."
+    JSON.parse(cloned_board)['id']
   end
 
   def close_default_lists_in(board)
@@ -59,37 +45,6 @@ class BoardDuplicator
   def clone_lists_from_to(original_board, cloned_board_id)
     original_board.lists.each do |list|
       client.post('/lists', name: list.name, idBoard: cloned_board_id, pos: 'bottom')
-    end
-  end
-
-  def clone_cards_from_to(original_board, cloned_board)
-    cloned_lists = []
-    cloned_board.lists.each do |list|
-      cloned_lists << list
-    end
-    original_board.lists.zip(cloned_lists).each do |list, cloned_list|
-      # puts "list: #{list}"
-      # puts "cloned_list: #{cloned_list}"
-      list.cards.each do |card|
-        # puts "card: #{card}"
-        # puts "cloned_list: #{cloned_list}"
-        original_card = JSON.parse(client.get("/cards/#{card.id}"))
-        # puts "original_card: #{original_card.class}"
-        # p original_card
-        # puts "card: #{card.class}"
-        # p card
-        # p original_card == card
-        # original_attachments = JSON.parse(client.get("/cards/#{card.id}/attachments"))
-        JSON.parse(client.post("/lists/#{cloned_list.id}/cards", original_card))
-        # puts original_attachments
-        # client.post("/lists/#{cloned_list.id}/cards")
-        # id = cloned_card['id']
-        # puts "id: #{id}"
-        # puts original_attachments['id']
-        # puts original_attachments['url']
-        # puts original_attachments['name']
-        # client.post("/cards/#{id}/attachments", url: original_attachments['url'], name: original_attachments['name'])
-      end
     end
   end
 
@@ -106,42 +61,58 @@ class BoardDuplicator
     token
   end
 
-  def subscribe_me_to_users_done_list(done_list, board_id = '', member = '')
-    if done_list
-      subscribe_member(done_list.id)
-    else
-      done_list = Trello::List.create(name: 'Done', board_id: board_id)
-      subscribe_member(done_list.id)
+  def subscribe_me_to_chosen_lists_in(cloned_board_id)
+    cloned_board = Trello::Board.find(cloned_board_id)
+    cloned_lists = {}
+    client.find(:board, cloned_board_id).lists.map do |list|
+      cloned_lists[list.name.downcase] = list.id
     end
-    puts "You have been subscribed to the list '#{done_list.name}' of user #{member.strip}."
-    p done_list.board_id
+    LISTS_TO_SUBSCRIBE_TO.each do |list|
+      if cloned_lists.keys.include?(list.downcase)
+        subscribe_member(cloned_lists[list.downcase])
+        puts "You have been subscribed to the list #{list} in the board #{cloned_board.name}."
+      else
+        new_list_id = JSON.parse(client.post("/boards/#{cloned_board_id}/lists", name: list, pos: 'bottom' ))['id']
+        subscribe_member(new_list_id)
+        puts "You have been subscribed to the newly created list #{list} in the board #{cloned_board.name}."
+      end
+    end
+
+    # (&:name).map(&:downcase)
+
+    # p list_names
+
+    # subscribe_to = (LISTS_TO_SUBSCRIBE_TO & list_names)
+    # client.find(:board, cloned_board_id).lists
+    # subscribe_to.each { |list_id| subscribe_member(list_id) }
+
+    # create_and_subscribe_to = LISTS_TO_SUBSCRIBE_TO - list_names
+    # if create_and_subscribe_to.any?
+    #   create_and_subscribe_to.each do |list_name|
+    #     new_list_id = JSON.parse(client.post("/boards/#{cloned_board_id}/lists", name: list_name, pos: 'bottom' ))['id']
+    #     subscribe_member(new_list_id)
+    #   end
+    # end
+
+    # else
+    #   done_list = Trello::List.create(name: 'Done', board_id: board_id)
+    #   subscribe_member(done_list.id)
+    # end
+    # p done_list.board_id
   end
 
-  def add_member(board_id, member_nickname)
-    client = Trello.client
-    member = Trello::Member.find(member_nickname)
-    path = "/boards/#{board_id}/members/#{member.id}"
-    client.put(path, type: 'normal')
+  def add_member(board_id, trello_nickname)
+    member = Trello::Member.find(trello_nickname)
+    client.put("/boards/#{board_id}/members/#{member.id}", type: 'normal')
+    board_name = client.find(:board, board_id).name
+    puts "Member #{trello_nickname} has been added to the board #{board_name}."
   end
 
   def subscribe_member(done_list_id)
-    path = "/lists/#{done_list_id}/subscribed"
-    client.put(path, value: true)
+    client.put("/lists/#{done_list_id}/subscribed", value: true)
   end
 
   def client
     client ||= Trello.client
   end
 end
-
-
-# close a board: client.put("/boards/xdJ6Icuq", {closed: true})
-
-# .scan(/(?<="name":")(.{8})/).flatten
-      # if client.get("/boards/#{token}").include?("closed\":true") == false
-
-    # %r{.*trello.com/b/(?<token>.*)/.*} =~ 'https://trello.com/b/khsx7Ez1/tat'
-
-    # p board_users_copy.lists.map(&:name).any? { |e| e =~ /done/i }
-    # p board_users_copy.lists.find()
-    # p board_users_copy.lists.pluck(:name).include?(/done/i)
