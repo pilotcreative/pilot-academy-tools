159970549eec7bbe96af5325ac00aacc71001f1d
mdkalish <mdkalish4git@gmail.com>
2015-03-11 14:13:49 +0100

    Add RSpecs for Authorization

--- spec/authorization_spec.rb
+++ spec/authorization_spec.rb
@@ -8,16 +8,16 @@ describe TrelloAutomation::AutomationManager do
 
   describe 'Authorization' do
     before do
-      Trello.configuration.member_token = nil
-      Trello.configuration.developer_public_key = nil
       stub_const('Constants::DEVELOPER_PUBLIC_KEY', 'dpk')
       stub_const('Constants::APP_NAME', 'Test App Name')
+      Authorization.stub(:token_file_path) { TEST_FILE }
     end
 
     context 'when there is token file in system' do
       before do
+        Trello.configuration.member_token = nil
+        Trello.configuration.developer_public_key = nil
         File.open(TEST_FILE, 'w') { |f| f.write "member_token: #{MEMBER_TOKEN}" }
-        Authorization.stub(:token_file_path) { TEST_FILE }
       end
 
       after do
@@ -49,31 +49,55 @@ describe TrelloAutomation::AutomationManager do
 
     context 'when there is no token file in system' do
       before do
-        @url = Authorization.member_token_url
-        Authorization.stub(:token_file_path) { TEST_FILE }
-        $stdout = StringIO.new
-        Authorization.open_url_in_browser(@url, dry_run: true)
-      end
-
-      after do
-        $stdout = STDOUT
+        allow(Authorization).to receive(:read_token_from_stdin).and_return(MEMBER_TOKEN)
+        File.delete(TEST_FILE) if File.exist?(TEST_FILE)
+        expect(File.exist?(TEST_FILE)).to be_falsey
       end
 
       it 'opens browser to deliver the key' do
-        expect(browsers.any? { |b| $stdout.string =~ /#{b}/ }).to be_truthy
-        expect($stdout.string).to include('https://')
+        expect(Authorization).to receive(:open_url_in_browser)
+        Authorization.authorize
       end
 
-      xit 'prompts user for the key' do
-      end
+      describe 'and stores the key from STDIN to file' do
+        before do
+          Authorization.stub(:open_url_in_browser)
+        end
 
-      xit 'creates token file in system' do
-      end
+        it 'prompts user for the key' do
+          expect(Authorization).to receive(:read_token_from_stdin)
+          Authorization.authorize
+        end
 
-      xit 'saves the key to the token file in system' do
+        it 'creates token file in system' do
+          expect(File.exist?(TEST_FILE)).to be_falsey
+          Authorization.authorize
+          expect(File.exist?(TEST_FILE)).to be_truthy
+        end
+
+        it 'saves the key to the token file in system' do
+          expect(File.exist?(TEST_FILE)).to be_falsey
+          Authorization.authorize
+          expect(File.open(TEST_FILE, 'r') { |f| f.read }).to include(MEMBER_TOKEN)
+        end
       end
 
       describe 'Trello App URL' do
+        before do
+          url = Authorization.member_token_url
+          $stdout = StringIO.new
+          Authorization.open_url_in_browser(url, dry_run: true)
+        end
+
+        after do
+          $stdout = STDOUT
+        end
+
+        it 'is streamed to $stdout by Launchy when dry_run: true' do
+          expect(browsers.any? { |b| $stdout.string =~ /#{b}/ }).to be_truthy
+          expect($stdout.string).to include('https://')
+        end
+
         it 'includes correct DEVELOPER_PUBLIC_KEY' do
           expect($stdout.string).to include("key=#{Constants::DEVELOPER_PUBLIC_KEY}")
         end
@@ -82,11 +106,11 @@ describe TrelloAutomation::AutomationManager do
           expect($stdout.string).to include("name=#{Constants::APP_NAME}")
         end
 
-        it 'queries response type = token' do
+        it "queries 'response_type=token'" do
           expect($stdout.string).to include('response_type=token')
         end
 
-        it 'queries scope = read and write' do
+        it "queries 'scope=read,write'" do
           expect($stdout.string).to include('scope=read,write')
         end
       end
